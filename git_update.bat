aa815beeddd2b31b1b37f213fec23ca039c5fae6
Milan Milinkovic <Milan Milinkovic>
2017-03-27 01:13:33 +0200

    2nd Hands On

---
+++ git_update.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "2nd Hands On"
+git push -u origin master
---
+++ tut_4/Apples.java
@@ -0,0 +1,10 @@
+package tut_4;
+
+public class Apples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Youtube!!!");
+	}
+
+}
