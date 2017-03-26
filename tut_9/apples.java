835aff49e88f96679792a943b2120f2eb0a78c5f
Milan Milinkovic <Milan Milinkovic>
2017-03-27 01:43:56 +0200

    Automated Commit

---
+++ tut_9/apples.java
@@ -0,0 +1,20 @@
+package tut_9;
+
+public class apples {
+
+	public static void main(String[] args) {
+		
+		int tuna1 = 5;
+		int tuna2 = 5;
+		
+		System.out.println(++tuna1); // First Update the Variable, Then Use
+		System.out.println(  tuna1); // 
+
+		System.out.println(tuna2--); // First Use the Variable, Then Update
+		System.out.println(  tuna2); // 
+		
+		tuna1 *= 10;                 // Assignment Tutorials
+		System.out.println(  tuna1); // 
+	}
+
+}
